fce38ca5bc59b97df5b654ba826b88c1bac81f39
Your Name <johnkamau106@gmail.com>
2025-06-21 21:43:37 +0300

    fix: correct appearance creation logic and variable name in seed script

--- late-show-api-challenge/server/seed.py
+++ late-show-api-challenge/server/seed.py
@@ -4,3 +4,32 @@ from server.models.guest import Guest
 from server.models.episode import Episode
 from server.models.appearance import Appearance
 
+app = create_app()
+
+with app.app_context():
+    db.drop_all:
+    db.create_all()
+
+    # Create default user
+    user = User(username="admin")
+    user = set_password("password")
+    db.session.add(user)
+
+    # Create default guests
+    guest1 = guest(name="Lylla", occupation="Actress")
+    guest2 = guest(name="Bob collymore", occupation="Enterpreneur")
+    db.session.add_all([guest1, guest2])
+
+    # Create default episodes
+    episode1 = Episode(date="2024-06-12", number=1)
+    episode2 = Episode("2024-06-13", number=2)
+    db.session.add_all([episode1, episode2])
+    db.session.commit()
+
+    # Create default appearances
+    appearance1 = Appearance(rating=5, guest_id=guest1.id, episode_id=episode1.id)
+    appeaarance2 = Appearance(rating=6, guest_id=guest2.id, episode_id=episode2.id)
+    db.session.add_all([appearance1, appearance2])
+
+    db.session.commit()
+    print("Database seeded successfully.")
