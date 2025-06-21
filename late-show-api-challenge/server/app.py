3f7973292641ae53bc5c870d8acdd0e8e4adf0b7
Your Name <johnkamau106@gmail.com>
2025-06-21 21:16:32 +0300

    fix: correct typo in blueprint registration for auth controller

--- late-show-api-challenge/server/app.py
+++ late-show-api-challenge/server/app.py
@@ -16,6 +16,17 @@ def create_app():
     migrate.init_app(app, db)
     jwt.init_app(app)
 
-    
+    from server.controllers.auth_controller import auth_bp
+    from server.controllers.guest_controller import guest_bp
+    from server.controllers.episode_controller import episode_bp
+    from server.controllers.appearance_controller import appearance_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(guest_bp)
+    app.register_blueprint(episode_bp)
+    app.register_blueprint(appearance_bp)
+
+
+
 
     return app
